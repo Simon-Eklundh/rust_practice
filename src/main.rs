6ce595524344ae13301a87872aed99e08409d459
Simon Eklundh <simon.eklundh@gmail.com>
2022-08-28 00:50:19 +0200

    created a grep program in minigrep

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-mod lib;
 mod project1;
-
 use project1::guessing_game_main;
+
 use std::io;
 
 /**
@@ -11,7 +10,7 @@ fn main() {
     loop {
         println!("Please select a project to run:");
         println!("1. Guess the number");
-        println!("2. project 2");
+        println!("2. grep");
         println!("3. project 3");
         println!("4. Quit");
         let mut selection = String::new();
@@ -24,14 +23,14 @@ fn main() {
         };
         match selection {
             1 => guessing_game_main::start(),
-            2 => println!("Project2"),
+            2 => println!(
+                "project 2 is minigrep, run it from the terminal: with\
+            cargo run -- search_string file_name.txt"
+            ),
             3 => println!("Project 3"),
             4 => break,
             _ => continue,
         }
     }
     println!("Thank you for using this program!");
-}
-pub fn test_function() -> i32 {
-    50
 }
